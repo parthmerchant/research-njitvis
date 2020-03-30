b08f2b297dff9e4f5ee0e3f047eb4dca941b9b9d
Parth Merchant <32684552+parthmerchant@users.noreply.github.com>
2020-03-30 17:23:21 -0400

    Add files via upload
    Energy Viz Version 2.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import FirstViz from './components/FirstViz';
+import SecondViz from './components/SecondViz'
+import ThirdViz from './components/ThirdViz';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <FirstViz resumeData={resumeData}/>
+        <SecondViz resumeData={resumeData}/>
+        <ThirdViz resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
