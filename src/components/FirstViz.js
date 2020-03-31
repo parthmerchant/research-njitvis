a1b46213ad847cbb0518e56b0458431654e932fc
Parth Merchant <32684552+parthmerchant@users.noreply.github.com>
2020-03-31 16:24:15 -0400

    Add files via upload

--- src/components/FirstViz.js
+++ src/components/FirstViz.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
 export default class FirstViz extends Component {
   render() {
-    let resumeData = this.props.resumeData;
     return (
       <section id="about">
          <div className="row">
-
-            <div className="three columns">
-
-               
-
-            </div>
-
+            <div className="three columns"></div>
             <div className="nine columns main-col">
-
                <h2>Interactive Visualization 1</h2>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Component Details:</h2>
-                  
-                  </div>
-               </div>
             </div>
          </div>
       </section>
