2179a4a5e56b8179fec61c7eea01ff8301a3dbbf
Parth Merchant <32684552+parthmerchant@users.noreply.github.com>
2020-05-15 13:45:38 -0400

    Add files via upload

--- src/components/SecondViz.js
+++ src/components/SecondViz.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import data from '../datasets/United Nations Energy Census/all_energy_statistics.csv';
-
-d3.csv(data).then(function(data) {
-  console.log(data);
-});
+// import data from '../datasets/United Nations Energy Census/all_energy_statistics.csv';
 
 export default class SecondViz extends Component {
   render() {
